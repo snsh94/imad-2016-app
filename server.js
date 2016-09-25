f1785f0e7b1c717a54bf55a36ba4d25885f068e5
snsh94 <iamnishh94@gmail.com>
2016-09-25 21:20:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function (req,res){
+    res.send('ARTICLE-ONE');
+});
+app.get('/article-two',function (req,res){
+    res.send('ARTICLE-TWO');
+});
+app.get('/article-three',function (req,res){
+    res.send('ARTICLE-THREE');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
